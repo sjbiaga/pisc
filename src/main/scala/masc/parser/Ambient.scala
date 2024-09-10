d99bc40b2dc3c17b9daa1ad42866d2d7abbe68f1
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2024-09-10 18:52:33 +0300

    added warning for recursive replication

--- src/main/scala/masc/parser/Ambient.scala
+++ src/main/scala/masc/parser/Ambient.scala
@@ -29,13 +29,13 @@
 package masc
 package parser
 
-import scala.collection.mutable.{ LinkedHashSet => Set }
+import scala.collection.mutable.{ HashMap => Map, LinkedHashSet => Set }
 import scala.io.Source
 
 import scala.util.parsing.combinator._
 
 import Ambient._
-import Calculus.Bind
+import Calculus.{ Bind, `(*)` }
 
 import scala.meta.{ Enumerator, Term }
 
@@ -97,11 +97,19 @@ object Ambient extends Calculus:
 
     if i < 0 then throw MainParsingException
 
-    given rec: Set[(String, Int)] = Set()
+    given rec: Map[(String, Int), Int] = Map()
+    given rep: Map[Int, Int] = Map()
 
     recursive(prog(i)._2)(using "Main" -> 0 :: Nil)
 
     if rec.contains("Main" -> 0) then throw MainParsingException2
+
+    for
+      (i, n) <- rep
+    do
+      prog(i)._1 match
+        case `(*)`(id, _, params*) =>
+          Console.err.println("Warning! " + RecRepParsingException(id, params.size, n).getMessage + ".")
 
   def apply(prog: List[Bind]): Unit =
 
