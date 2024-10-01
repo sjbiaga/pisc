27eeb0b957b91afecf5138fe2ad2a64037aafbb0
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2024-10-01 20:28:37 +0300

    added support for recursive encodings

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ The BNF formal grammar for processes is the following.
 
     LINE       ::= EQUATION | DEFINITION
     EQUATION   ::= AGENT "=" CHOICE
-    DEFINITION ::= "[" [ NUMBER ] "|" TEMPLATE "|" [ NUMBER ] "]" "=" CHOICE
+    DEFINITION ::= "[<NUMBER>|" TEMPLATE "|<NUMBER>]" "=" CHOICE
     CHOICE     ::= PARALLEL { "+" PARALLEL }
     PARALLEL   ::= SEQUENTIAL { "|" SEQUENTIAL }
     SEQUENTIAL ::= PREFIXES [ LEAF | "(" CHOICE ")" ]
@@ -50,7 +50,7 @@ The BNF formal grammar for processes is the following.
                  | IDENTIFIER "{" NAMES "}"
                  | AGENT
                  | EXPANSION
-    EXPANSION  ::= "[" [ NUMBER ] "|" EXPAND "|" [ NUMBER ] "]"
+    EXPANSION  ::= "[<NUMBER>|" EXPAND "|<NUMBER>]"
     AGENT      ::= [ QUAL ] IDENTIFIER [ "(" NAMES ")" ]
 
 The BNF formal grammar for prefixes is the following.
--- src/main/scala/pisc/parser/PolyadicPi.scala
+++ src/main/scala/pisc/parser/PolyadicPi.scala
@@ -105,6 +105,7 @@ class PolyadicPi extends Expression:
 
   private[parser] var eqtn = List[Bind]()
   private[parser] val defn = Map[Int, Define]()
+  private[parser] val self = Set[Int]()
 
 
 object PolyadicPi extends Extension:
@@ -175,6 +176,8 @@ object PolyadicPi extends Extension:
     ensure(prog)
 
 
+  private var i: Int = 0
+
   def apply(source: Source): List[Either[String, Bind]] = (source.getLines().toList :+ "")
     .foldLeft(List[String]() -> false) {
       case ((r, false), l) => (r :+ l) -> l.endsWith("\\")
@@ -190,11 +193,19 @@ object PolyadicPi extends Extension:
         parseAll(line, it) match
           case Success(Left(equation), _) =>
             eqtn :+= equation
-            Some(Right(equation))
+            val equations = eqtn.slice(i, eqtn.size)
+            i = eqtn.size
+            equations.map(Right(_))
           case Success(Right(definition @ (Encoding(code, _, _, _), _)), _) =>
             defn(code) = definition
-            None
+            Nil
           case failure: NoSuccess =>
             scala.sys.error(failure.msg)
+    }
+    .filter {
+      case Right((`(*)`(s"Self_$n", _, _*), _))
+          if { try { n.toInt; true } catch _ => false } =>
+        self.contains(n.toInt)
+      case _ => true
     }
     .toList
