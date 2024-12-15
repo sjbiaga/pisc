c5c4731d7de079af1b277400b5f3c1202f87a3a9
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2024-12-15 17:58:15 +0200

    fixed no exception in rename before expansion

--- src/main/scala/pixc/parser/Encoding.scala
+++ src/main/scala/pixc/parser/Encoding.scala
@@ -72,8 +72,9 @@ abstract class Encoding extends Calculus:
             if parameters.size == _parameters.size
             then
               eqtn :+= `(*)`("Self_" + _code, binding.map(λ(_)).toSeq*) -> sum
-            val `macro` = Macro(parameters.toList, _parameters.size, constants, variables, given_Names2, sum)
-            `macro` -> Definition(_code, term, constants, variables, sum)
+            Macro(parameters.toList, _parameters.size, constants, variables, given_Names2, sum)
+            ->
+            Definition(_code, term, constants, variables, sum)
         }
     }
 
@@ -202,12 +203,7 @@ object Encoding:
         }
       given Names2 = Names2(binding2)
       given Names()
-      val sum2 =
-        try
-          sum.rename(id, Set.empty, definition = true)
-        catch
-          case it: NoBPEx => throw NoBindingParsingException(_code, _nest, it.getMessage)
-          case it => throw it
+      val sum2 = sum.rename(id, Set.empty, definition = true)
       val shadows = (
         parameters.map(_ -> None).toMap
         ++
@@ -215,8 +211,9 @@ object Encoding:
       ) .toList
         .sortBy { (it, _) => parameters.indexOf(it) }
         .map(_._2)
-      val `def` = Definition(code, Some(term), constants, variables2, sum2)
-      `def` -> (arity - shadows.count(_.nonEmpty) -> shadows)
+      Definition(code, Some(term), constants, variables2, sum2)
+      ->
+      (arity - shadows.count(_.nonEmpty) -> shadows)
 
   case class Definition(code: Int,
                         term: Option[Term],
