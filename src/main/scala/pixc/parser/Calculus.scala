c0de029c00751eca3d4210252d783c2c576d8c18
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2025-01-09 17:48:35 +0200

    entered 2025

--- src/main/scala/pixc/parser/Calculus.scala
+++ src/main/scala/pixc/parser/Calculus.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Sebastian I. Gliţa-Catina <gseba@users.sourceforge.net>
+ * Copyright (c) 2023-2025 Sebastian I. Gliţa-Catina <gseba@users.sourceforge.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
