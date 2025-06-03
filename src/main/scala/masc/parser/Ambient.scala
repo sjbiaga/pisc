3554dcac7fb20aff8027b20609fc24b47abf4cc9
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2025-06-03 20:53:43 +0300

    fixed expansion no check for operator

--- src/main/scala/masc/parser/Ambient.scala
+++ src/main/scala/masc/parser/Ambient.scala
@@ -96,13 +96,13 @@ abstract class Ambient extends Expression:
 
   protected final def paste(it: Any) = _id.paste(it)
 
-  protected final def save[T](r: => (ParseResult[T], Any), fail: Boolean): Option[(T, Input)] =
+  protected final def save[T](r: => ParseResult[T], fail: Boolean): Option[(T, Input)] =
     val nest = _nest
     val cntr = Map.from(_cntr)
     _id.save {
       r match
-        case (Success(it, in), _) => Some(it -> in)
-        case (failure: NoSuccess, _) if fail =>
+        case Success(it, in) => Some(it -> in)
+        case failure: NoSuccess if fail =>
           scala.sys.error(failure.msg)
         case _ =>
          _cntr = cntr
