c9fe27f6f2aa5e353ba8a8bf912ea79e0abb729b
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2025-07-12 14:09:09 +0300

    bumped versions

--- bin/ma.sh
+++ bin/ma.sh
@@ -19,7 +19,7 @@ function ma() {
     set ../ma.scala ${srcs#?}
     scala-cli run "$@" \
                   -q -O -nowarn -S 3.7.2-RC1 \
-                  --dep org.typelevel::cats-effect:3.7-4972921 \
+                  --dep org.typelevel::cats-effect:3.6.2 \
                   -Dcats.effect.warnOnNonMainThreadDetected=false \
                   ${args#?} \
                   2>&1
