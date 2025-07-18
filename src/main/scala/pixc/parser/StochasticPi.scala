144b91266690d7df44f720a87a6daba7384b074d
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2025-07-18 18:13:27 +0300

    fixed τ label, added Branches list

--- src/main/scala/pixc/parser/StochasticPi.scala
+++ src/main/scala/pixc/parser/StochasticPi.scala
@@ -513,7 +513,9 @@ object StochasticPi:
             (`!`(Some(μ), it), Actions(μ))
 
           case !(_, sum) =>
-            `!`(Some(τ), sum).parse
+            val τʹ: τ = τ
+            def idʹ: String = '!' + τʹ.υidυ
+            `!`(Some(τʹ.copy()(idʹ)), sum).parse
 
           case `⟦⟧`(definition, variables, _sum, υidυ, name, assignment) =>
             val n = assignment.size
