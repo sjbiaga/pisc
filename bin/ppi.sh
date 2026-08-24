98d756a2de1d96127666e4e5f661133a8d6f0205
Sebastian Gliţa-Catina <sjb.ia.ga@gmail.com>
2026-08-24 12:48:59 +0300

    update versions

--- bin/ppi.sh
+++ bin/ppi.sh
@@ -26,10 +26,10 @@ function ppi() {
             ;;
         akka)
             local deps='--repo https://repo.akka.io/cAzJkaebGFNkNrv2ILttVDQWmf3u4ThOcE_EbfzM0-N8lDhx/secure
-                        --dep com.typesafe.akka::akka-actor-typed:2.10.20'
+                        --dep com.typesafe.akka::akka-actor-typed:2.10.21'
             ;;
         pekko)
-            local deps='--dep org.apache.pekko::pekko-actor-typed:1.6.0'
+            local deps='--dep org.apache.pekko::pekko-actor-typed:1.7.0'
             ;;
         fs2)
             local deps='--dep co.fs2::fs2-core:3.13.0
@@ -62,7 +62,7 @@ function ppi() {
     done
     set ${srcs#?} ../${emit}/ppi.scala
     scala-cli run "$@" $deps \
-                  -q -O -nowarn -S 3.9.0-RC4 \
+                  -q -O -nowarn -S 3.9.0-RC6 \
                   --dep eu.timepit::refined:0.11.4 \
                   ${args#?} \
                   2>&1
@@ -94,10 +94,10 @@ function ppi_() {
             ;;
         akka)
             local deps='--repo https://repo.akka.io/cAzJkaebGFNkNrv2ILttVDQWmf3u4ThOcE_EbfzM0-N8lDhx/secure
-                        --dep com.typesafe.akka::akka-actor-typed:2.10.20'
+                        --dep com.typesafe.akka::akka-actor-typed:2.10.21'
             ;;
         pekko)
-            local deps='--dep org.apache.pekko::pekko-actor-typed:1.6.0'
+            local deps='--dep org.apache.pekko::pekko-actor-typed:1.7.0'
             ;;
         fs2)
             local deps='--dep co.fs2::fs2-core:3.13.0
@@ -130,7 +130,7 @@ function ppi_() {
     done
     set ${srcs#?} ../${emit}/ppi_.scala
     scala-cli run "$@" $deps \
-                  -q -O -nowarn -S 3.9.0-RC4 \
+                  -q -O -nowarn -S 3.9.0-RC6 \
                   --dep eu.timepit::refined:0.11.4 \
                   ${args#?} \
                   2>&1
